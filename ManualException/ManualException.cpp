c452d8b283ad82647553564cd624abd57c8fcee9
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-06-21 11:23:50 +0700

    selesai membuat proggram manual exception

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -5,5 +5,15 @@ int main() {
 	try {
 		cout << "Selamat Belajar di Prodi TI UMY" << endl;
 		throw 0.5; //melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		//jila selain integer maka blok ini akan dieksekusi
+		cout << "default pengecualian dieksekusi" << endl;
+	}
+	return 0;
 }
